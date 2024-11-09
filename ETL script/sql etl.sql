7c50c6d4b63d41fcb3c040de8d77c2d46759d72d
Jhon Freddy Popo Moreno <52682209+jfpm@users.noreply.github.com>
2024-11-08 21:54:23 -0500

    ajuste eliminar base de de datos

--- ETL script/sql etl.sql
+++ ETL script/sql etl.sql
@@ -161,17 +161,13 @@ CREATE TABLE eficiencia_hora (
 
 ---- eliminar todos los registros
 
-DO $ $ DECLARE r RECORD;
-
-BEGIN FOR r IN (
-    SELECT
-        tablename
-    FROM
-        pg_tables
-    WHERE
-        schemaname = 'public'
-) LOOP EXECUTE 'TRUNCATE TABLE ' || quote_ident(r.tablename) || ' CASCADE;';
-
-END LOOP;
-
-END $ $;
+DO
+$$
+DECLARE
+    r RECORD;
+BEGIN
+    FOR r IN (SELECT tablename FROM pg_tables WHERE schemaname = 'public') LOOP
+        EXECUTE 'TRUNCATE TABLE ' || quote_ident(r.tablename) || ' CASCADE;';
+    END LOOP;
+END
+$$;
